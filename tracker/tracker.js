7b71497c5c916dbe10b4a784351a9ed2377c6b9f
amar kumar <amarkumar968507969@gmail.com>
2026-05-12 15:49:52 +0530

    feature: add demo site source code

---
+++ tracker/tracker.js
@@ -0,0 +1,53 @@
+(function () {
+  const API_URL = "http://localhost:5000/api/v1/events";
+
+  let sessionId = localStorage.getItem("sessionId");
+
+  if (!sessionId) {
+    sessionId = crypto.randomUUID();
+    localStorage.setItem("sessionId", sessionId);
+  }
+
+  function sendEvent(eventData) {
+    fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(eventData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Event Sent:", data);
+      })
+      .catch((err) => {
+        console.error("Tracking Error:", err);
+      });
+  }
+
+  // Track page view
+  sendEvent({
+    sessionId,
+    type: "page_view",
+    url: window.location.href,
+    timestamp: new Date().toISOString(),
+    userAgent: navigator.userAgent,
+    screenWidth: window.innerWidth,
+    screenHeight: window.innerHeight,
+  });
+
+  // Track clicks
+  document.addEventListener("click", (event) => {
+    sendEvent({
+      sessionId,
+      type: "click",
+      url: window.location.href,
+      timestamp: new Date().toISOString(),
+      x: event.clientX,
+      y: event.clientY,
+      userAgent: navigator.userAgent,
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight,
+    });
+  });
+})();
